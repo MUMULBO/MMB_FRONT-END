fbc1812562b20617de86f626ba495003b5257123
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-08 19:44:25 +0900

    init pages

---
+++ mmb_front/src/pages/PostModifyPage/PostModifyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostModifyPage = () => {
+    return (
+        <div>
+            질문글 수정 페이지
+        </div>
+    );
+};
+
+export default PostModifyPage;
---
+++ mmb_front/src/pages/PostWritePage/PostWritePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostWritePage = () => {
+    return (
+        <div>
+            질문글 작성 페이지
+        </div>
+    );
+};
+
+export default PostWritePage;
