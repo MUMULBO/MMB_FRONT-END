38a909d76eb7135f949b17feb8b880c9e3595380
GuHyeonWoo <ghw9174@gmail.com>
2022-07-09 19:16:30 +0900

    my_page 구현_미완

--- mmb_front/src/pages/MyPage/MyPage.js
+++ mmb_front/src/pages/MyPage/MyPage.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import './MyPage.css';
 
 const MyPage = () => {
     return (
-        <div>
-            마이 페이지
+        <div className='my_page'>
+            <div className='my_page_quoin'>
+                <div className='my_page_app_name'>
+                    MUMULBO
+                </div>
+                <div className='my_page_contents'>
+                    <div className='my_page_info'>
+                        <div className='my_img'>
+                            M
+                        </div>
+                        <div className='my_info'>
+                            <div className='my_info_nick'>
+                                닉네임
+                            </div>
+                            <div className='my_info_major'>
+                                컴퓨터공학과
+                            </div>
+                        </div>
+                        <div className='my_point'>
+                            <div className='my_point_img'>
+                                P
+                            </div>
+                            <div className='my_point_count'>
+                                263
+                            </div>
+                        </div>
+                    </div>
+                    <div className='my_page_menu'>
+                        <div className='my_created_posts'>
+                            <div className='my_created_toggle'>
+                                ?
+                            </div>
+                            <div className='my_created_title'>
+                                내가 작성한 질문글
+                            </div>
+                        </div>
+                        <div className='my_created_comments'>
+                            <div className='my_created_toggle'>
+                                -
+                            </div>
+                            <div className='my_created_title'>
+                                내가 작성한 댓글
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
