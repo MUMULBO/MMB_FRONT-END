c6a1e6df44d29dd492dbd05f9ef7ce4e668db3db
OhSeChan <seb_s19@naver.com>
2022-07-11 05:58:34 +0900

    major_id+2

--- mmb_front/src/pages/PostWritePage/PostWritePage.js
+++ mmb_front/src/pages/PostWritePage/PostWritePage.js
@@ -23,6 +23,15 @@ const PostWritePage = () => {
     //글 작성 완료 여부
     const [is_post,setIs_post] = useState(false);
 
+    //사용자 보유 포인트
+    const [user_point,setUser_point] = useState(0);
+    //페이지 시작 시 사용자 보유 포인트 가져오기
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:8000/postapp/upload/form')
+        .then((res)=>{setUser_point(res.data.point)})
+        .catch((err)=>{console.log(err)})
+    },[]);
+
     const selectList =
     ["일상","전자공학부","건축학부","산업공학부","화학소재공학부","신소재공학부","기계공학과","기계설계공학과","기계시스템공학과",
     "토목공학과","컴퓨터공학과","컴퓨터소프트웨어공학과","인공지능공학과","광시스템공학과","메디컬IT융합공학과","환경공학과",
@@ -159,7 +168,7 @@ const PostWritePage = () => {
                                 </select>
                                     <span className = "point"></span>
                                 </div>
-                                <p className = "nowPoint">{`현재 포인트 ${post_point}point`}</p>
+                                <p className = "nowPoint">{`보유 포인트 ${user_point}point`}</p>
                             </div>
                         </div>
                     </div>
